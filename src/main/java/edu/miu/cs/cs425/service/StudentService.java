4acdbdea762a832c981b8b9fd305136d60fbba29
Mussie <mussiekahsay591@gmail.com>
2024-07-26 18:15:59 -0400

    StudentService added

---
+++ src/main/java/edu/miu/cs/cs425/service/StudentService.java
@@ -0,0 +1,7 @@
+package edu.miu.cs.cs425.service;
+
+public interface StudentService {
+
+
+
+}
---
+++ src/main/java/edu/miu/cs/cs425/service/StudentServiceImpl.java
@@ -0,0 +1,9 @@
+package edu.miu.cs.cs425.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentServiceImpl implements StudentService{
+
+
+}
