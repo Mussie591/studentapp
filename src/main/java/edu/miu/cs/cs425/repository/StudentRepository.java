581c56550f5da57511d1baa7a32998389acb44e6
Semereande <andesema06@gmail.com>
2024-07-26 18:51:41 -0500

    studentRepository query added

--- src/main/java/edu/miu/cs/cs425/repository/StudentRepository.java
+++ src/main/java/edu/miu/cs/cs425/repository/StudentRepository.java
@@ -1,10 +1,17 @@
 package edu.miu.cs.cs425.repository;
 
+import edu.miu.cs.cs425.model.Course;
 import edu.miu.cs.cs425.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student,Integer> {
+    @Query("SELECT s.courses FROM Student s WHERE s.Id = :studentId")
+    List<Course> findCoursesByStudentId(@Param("studentId") int studentId);
 
 }
