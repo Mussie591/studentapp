433fbba4fd221b21e32a94c6c827320512b903c9
nahom <nbirhane@gmail.com>
2024-07-26 17:00:04 -0500

    Initial cut

---
+++ src/main/java/edu/miu/cs/cs425/model/Address.java
@@ -0,0 +1,23 @@
+package edu.miu.cs.cs425.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "address_id")
+    private Integer id;
+    private String city;
+    private String state;
+    private String zip;
+}
