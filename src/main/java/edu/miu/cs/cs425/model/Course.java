e7a4f34cbf5956ef1a5867ea7a1378d1fb0253aa
Mussie <mussiekahsay591@gmail.com>
2024-07-26 17:32:00 -0400

    Course added and completed

--- src/main/java/edu/miu/cs/cs425/model/Course.java
+++ src/main/java/edu/miu/cs/cs425/model/Course.java
@@ -1,11 +1,25 @@
 package edu.miu.cs.cs425.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
 public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "course_id")
+    private Integer id;
+    private String name;
+
+    @ManyToMany(mappedBy = "courses")
+    private List<Student> students;
 
 }
--- src/main/java/edu/miu/cs/cs425/repository/StudentRepository.java
+++
@@ -1,4 +0,0 @@
-package edu.miu.cs.cs425.repository;
-
-public class StudentRepository {
-}
