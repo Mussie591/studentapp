2ed314485071b973fa54b3019ad6e21a4d7775e7
michal tesfu <michaltesfu2@gmail.com>
2024-07-26 17:44:57 -0500

    still working in project

--- src/main/java/edu/miu/cs/cs425/controller/CourseDetailsController.java
+++ src/main/java/edu/miu/cs/cs425/controller/CourseDetailsController.java
@@ -1,16 +1,42 @@
 package edu.miu.cs.cs425.controller;
 
-import edu.miu.cs.cs425.service.CourseDetailsService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import edu.miu.cs.cs425.model.CourseDetails;
+import edu.miu.cs.cs425.service.CourseDetailsImpl;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/coursedetails")
 public class CourseDetailsController {
-    private final CourseDetailsService courseDetailsService;
+    private final CourseDetailsImpl courseDetailsService;
 
-    public CourseDetailsController(CourseDetailsService courseDetailsService) {
+    public CourseDetailsController(CourseDetailsImpl courseDetailsService) {
         this.courseDetailsService = courseDetailsService;
     }
+    @GetMapping
+    public CourseDetails getCourseDetails(CourseDetails courseDetails) {
+        return courseDetailsService.getCourseDetails(courseDetails);
+    }
+    @GetMapping("/all")
+    public Iterable<CourseDetails> getAllCourseDetails() {
+        return courseDetailsService.getAllCourseDetails();
+    }
+
+    @PostMapping
+    public void saveCourseDetails(CourseDetails courseDetails) {
+      courseDetailsService.saveCourseDetails(courseDetails);
+    }
+    @GetMapping("/course")
+    public Iterable<CourseDetails> getCourseDetailsByCourse(CourseDetails courseDetails) {
+        return courseDetailsService.getCourseDetailsByCourse(courseDetails);
+    }
+    @DeleteMapping("/{descriptionId}")
+    public void deleteCourseDetails(@PathVariable Integer descriptionId) {
+        courseDetailsService.deleteCourseDetails(descriptionId);
+    }
+    @PutMapping
+    public void updateCourseDetails(CourseDetails courseDetails) {
+         courseDetailsService.updateCourseDetails(courseDetails);
+    }
+
 
 }
