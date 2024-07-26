2570874dd5758e359bf538f8de2497e1f396f5a8
nahom <nbirhane@gmail.com>
2024-07-26 17:38:44 -0500

    init

--- src/main/java/edu/miu/cs/cs425/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.miu.cs.cs425.repository;
-
-import java.util.List;
-
-public class StudentRepository {
-
-    List<Student> findAllStudentsByProgramAndGpaLessThan(String program, double gpa);
-}
